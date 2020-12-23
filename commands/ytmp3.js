9e4920e4a3b9af42a91ef822c1118e870c104697
youbyoub <ayosiiiofficiel@gmail.com>
2020-12-23 11:28:40 +0100

    WMStrm is now usable as a module

--- commands/ytmp3.js
+++ commands/ytmp3.js
@@ -19,75 +19,48 @@ exports.run = async function (client, message, args) {
 
     if (!isLink) return client.reply(message.from, 'C\'est pô un lien valide ça !', message.id)
 
-    try {
-        client.reply(message.from, '_J\'y travaille..._', message.id);
-        let videoURL = args[0].toString();
-        var videoInfo = (await ytdl.getInfo(videoURL))
-        var videoTitle = videoInfo.videoDetails.title.replace('|', '').toString('ascii');
+    //try {
+    client.reply(message.from, '_J\'y travaille..._', message.id);
+    let videoURL = args[0].toString();
+    var videoInfo = (await ytdl.getInfo(videoURL))
+    var videoTitle = videoInfo.videoDetails.title.replace('|', '').toString('ascii');
 
-        if (videoInfo.videoDetails.lengthSeconds > 5 * 60) return client.reply(message.from, '*La vidéo est trop longue (>5 min) !*\n_Ne t\'inquiètes pas, c\'est ce qu\'elles disent toutes ;)_', message.id)
+    if (videoInfo.videoDetails.lengthSeconds > 5 * 60) return client.reply(message.from, '*La vidéo est trop longue (>5 min) !*\n_Ne t\'inquiètes pas, c\'est ce qu\'elles disent toutes ;)_', message.id)
 
-        await client.sendFileFromUrl(message.from, videoInfo.videoDetails.thumbnail.thumbnails[2].url,
-            './temp/thumb.jpg', `➸ *Titre* : ${videoTitle}\n\n◌ Le fichier arrive 🦅 !`)
-            .catch(() => client.reply(message.from, 'Erreur image :( '));
+    await client.sendFileFromUrl(message.from, videoInfo.videoDetails.thumbnails[2].url,
+        './temp/thumb.jpg', `➸ *Titre* : ${videoTitle}\n\n◌ Le fichier arrive 🦅 !`)
+        .catch(() => client.reply(message.from, 'Erreur image :( '));
 
-        var videoReadableStream = ytdl(videoURL, { filter: 'audioonly', quality: 'lowest' });
+    var videoReadableStream = ytdl(videoURL, { filter: 'audioonly', quality: 'lowest' });
 
-        // need a queue system & then i can use id file caching
-        //var tempFile = `./temp/${message.chat.id}_${videoInfo.videoDetails.videoId}.mp3`;
-        //var videoWritableStream = fs.createWriteStream(tempFile);
-        //var stream = videoReadableStream.pipe(videoWritableStream);
-
-        var util = require('util');
-        var Writable = require('stream').Writable;
-
-
-        var memStore = {};
-
-        /* Writable memory stream */
-        function WMStrm(key, options) {
-            // allow use without new operator
-            if (!(this instanceof WMStrm)) {
-                return new WMStrm(key, options);
-            }
-            Writable.call(this, options); // init super
-            this.key = key; // save key
-            memStore[key] = Buffer.from(''); // empty
-        }
-        util.inherits(WMStrm, Writable);
-
-        WMStrm.prototype._write = function (chunk, enc, cb) {
-            // our memory store stores things in buffers
-            var buffer = (Buffer.isBuffer(chunk)) ?
-                chunk :  // already is Buffer use it
-                Buffer.from(chunk, enc);  // string, convert
-
-            // concat to the buffer already there
-            memStore[this.key] = Buffer.concat([memStore[this.key], buffer]);
-            cb();
-        };
-
-        // Trying our stream out
-        var wstream = new WMStrm('data');
-
-        var stream = await videoReadableStream.pipe(wstream);
-
-        stream.on('finish', async function () {
-            console.log('finished writing');
-            await client.sendAudio(message.from, `data:audio/mpeg;base64,${memStore.data.toString('base64')}`)
-                .catch((e) => client.reply(message.from, 'Erreur mp3 :( ', message.id) && console.log(e));
-            wstream.end();
-        });
+    // need a queue system & then i can use id file caching
+    //var tempFile = `./temp/${message.chat.id}_${videoInfo.videoDetails.videoId}.mp3`;
+    //var videoWritableStream = fs.createWriteStream(tempFile);
+    //var stream = videoReadableStream.pipe(videoWritableStream);
 
 
 
-        /*stream.on('finish', async function () {
-            await client.sendAudio(message.from, stream)
-                .catch(() => client.reply(message.from, 'Erreur mp3 :( ', message.id));
-        });*/
+    // Trying our stream out
+    var wstream = new (require('../utils/WMStrm'))('data');
 
-    } catch (error) {
+    var stream = await videoReadableStream.pipe(wstream);
+
+    stream.on('finish', async function () {
+        console.log('finished writing');
+        await client.sendAudio(message.from, `data:audio/mpeg;base64,${wstream._memStore.data.toString('base64')}`)
+            .catch((e) => client.reply(message.from, 'Erreur mp3 :( ', message.id) && console.log(e));
+        wstream.end();
+    });
+
+
+
+    /*stream.on('finish', async function () {
+        await client.sendAudio(message.from, stream)
+            .catch(() => client.reply(message.from, 'Erreur mp3 :( ', message.id));
+    });*/
+
+    /*} catch (error) {
         client.reply(message.from, '*Erreur!* Le lien n\'est sûrement pas valide !', message.id)
         console.log(client.utils.moment().format("H:mm:ss") + ' *Erreur!* Le lien n\'est sûrement pas valide !' + error)
-    }
+    }*/
 }
--- utils/WMStrm.js
+++ utils/WMStrm.js
@@ -1,2 +1,45 @@
-// took this incredible code from http://codewinds.com/blog/2013-08-19-nodejs-writable-streams.html#storing_data_with_writable_streams
+// mostly based on this incredible code from http://codewinds.com/blog/2013-08-19-nodejs-writable-streams.html#storing_data_with_writable_streams
 
+var util = require('util');
+var Writable = require('stream').Writable;
+var memStore = {};
+
+/* Writable memory stream */
+function WMStrm(key, options) {
+    // allow use without new operator
+    if (!(this instanceof WMStrm)) {
+        return new WMStrm(key, options);
+    }
+    Writable.call(this, options); // init super
+    this.key = key; // save key
+    memStore[key] = Buffer.from(''); // empty
+}
+util.inherits(WMStrm, Writable);
+
+WMStrm.prototype._write = function (chunk, enc, cb) {
+    // our memory store stores things in buffers
+    var buffer = (Buffer.isBuffer(chunk)) ?
+        chunk :  // already is Buffer use it
+        Buffer.from(chunk, enc);  // string, convert
+
+    // concat to the buffer already there
+    memStore[this.key] = Buffer.concat([memStore[this.key], buffer]);
+    cb();
+};
+
+// to access memory from instance
+WMStrm.prototype._memStore = memStore;
+
+module.exports = WMStrm;
+
+
+/*
+var wstream = new (require('../utils/WMStrm'))('data');
+var stream = await videoReadableStream.pipe(wstream);
+
+stream.on('finish', async function () {
+    console.log('finished writing');
+    await client.sendAudio(message.from,
+        `data:audio/mpeg;base64,${wstream._memStore.data.toString('base64')}`).catch((e) => console.log(e));
+    wstream.end();
+});*/
