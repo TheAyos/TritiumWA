cc43ea34206a3bcec90c3b9a4f56e626071c6700
youbyoub <ayosiiiofficiel@gmail.com>
2020-12-24 08:01:57 +0100

    basic sticker command

--- commands/sticker.js
+++ commands/sticker.js
@@ -14,20 +14,20 @@ exports.run = async function (client, message, args) {
     const fs = require('fs');
 
     try {
-        client.reply(message.from, 'Ton sticker arrive 🦅 !', message.id);
-        const isQuotedImage = message.quotedMsg && message.quotedMsg.type === 'image'
 
+        const isQuotedImage = message.quotedMsg && message.quotedMsg.type === 'image'
         if ((message.isMedia || isQuotedImage) && args.length === 0) {
+            client.reply(message.from, 'Ton sticker arrive 🦅 !', message.id);
             const encryptMedia = isQuotedImage ? message.quotedMsg : message
             const _mimetype = isQuotedImage ? message.quotedMsg.mimetype : message.mimetype
             const mediaData = await decryptMedia(encryptMedia)
             const imageBase64 = `data:${_mimetype};base64,${mediaData.toString('base64')}`
-            const fileName = `./${message.from}.webp`
-            await fs.writeFileSync(fileName, mediaData)
-            await exec(`webpmux -set exif STK-20201118-WA0114.webp ${fileName} -o ${fileName}`)
+            //const fileName = `./${message.from}.webp`
+            //await fs.writeFileSync(fileName, mediaData)
+            //await exec(`webpmux -set exif STK-20201118-WA0114.webp ${fileName} -o ${fileName}`)
             //const contents = await fs.readFileSync(`./${message.from}.webp`, { encoding: 'base64' })
-            const contents = await fs.readFileSync(fileName, { encoding: 'base64' })
-            await client.sendRawWebpAsSticker(message.from, contents);
+            //const contents = await fs.readFileSync(fileName, { encoding: 'base64' })
+            await client.sendImageAsSticker(message.from, imageBase64);
             client.reply(message.from, 'Here\'s your sticker');
             //await client.sendRawWebpAsSticker(message.from, contents)
             //client.reply(message.from, 'Here\'s your sticker')
