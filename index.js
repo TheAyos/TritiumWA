c5abd456fd852f6b65c24e001ae8d793de14344a
youbyoub <ayosiiiofficiel@gmail.com>
2020-12-24 15:42:07 +0100

    working on better command handling

--- index.js
+++ index.js
@@ -8,28 +8,27 @@ const msgHandler = require('./handler/handler')
 const utils = require('./utils/utils')
 
 /// WOOWOWOWO RED COLOR MAMAMIAAAA
-console.log("\x1b[1m\x1b[31m\x1b[40m\x1b[1m\x1b[31m\x1b[40m" + `WIN32 IS NOT OFFICIALLY SUPPORTED!
-Although there's a (very) slim chance of it working, multiple aspects of the bot are built with UNIX-like systems in mind and could break on Win32-based systems. If you want to run the bot on Windows, using Windows Subsystem for Linux is highly recommended.
-The bot will continue to run past this message, but keep in mind that it could break at any time. Continue running at your own risk; alternatively, stop the bot using Ctrl+C and install WSL.` + "\x1b[0m");
+console.log("\x1b[1m\x1b[31m\x1b[40m" + `should be red text` + "\x1b[0m"); console.log("should be white");
 
 function start(client) {
 
   client.config = config;
-  client.prefix = config.prefix;
+  client.prefix = config.default_prefix;
   client.utils = utils;
 
-
-
   console.log('[DEV] Tritium');
-  console.log('[CLIENT] Client Started! with prefix -> \'' + client.prefix + '\' \n\n');
-
+  console.log('[CLIENT] Client Started!\n ' +
+    `default prefix ->  '${client.prefix}' \n\n` +
+    `default prefix ->  '${client.prefix}' \n\n`);
 
 
   client.commands = new Enmap();
   client.aliases = new Enmap();
-  client.helps = new Enmap();
 
   require('./CommandLoader')(client)
+
+
+  // handle events separately after
 
   // Log all messages
   client.onAnyMessage((msg) => {
