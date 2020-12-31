9713e09048bc4497065c75881f5f6d254f5ed114
youbyoub <ayosiiiofficiel@gmail.com>
2021-01-01 00:15:33 +0100

    cleanup

--- utils/functions.js
+++ utils/functions.js
@@ -1,29 +1,60 @@
 module.exports = (Tritium) => {
-    Tritium.rootPath = require("path").resolve();
-    Tritium.fromRootPath = (...pathS) => require("path").join(Tritium.rootPath, ...pathS);
-    Tritium.getCommand = function (args) {
-        return args.triggers
-            ? Tritium.commands.find((cmd) => cmd.triggers.includes(args.triggers[0]))
-            : Tritium.commands.find((cmd) => cmd.triggers.includes(args));
-    };
+  Tritium.rootPath = require("path").resolve();
+  Tritium.fromRootPath = (...pathS) => require("path").join(Tritium.rootPath, ...pathS);
+  Tritium.getCommand = function (args) {
+    return typeof args === "string"
+      ? Tritium.commands.find((cmd) => cmd.triggers.includes(args))
+      : Tritium.commands.find((cmd) => cmd.triggers.includes(args.triggers[0]));
+  };
 
-    Tritium.getCmdAliases = function (args) {
-        const cmdTriggers = Tritium.getCommand(args).triggers;
-        return cmdTriggers.filter((alias) => alias != cmdTriggers[0]);
-    };
+  Tritium.getCmdAliases = function (args) {
+    const cmdTriggers = Tritium.getCommand(args).triggers;
+    return cmdTriggers.filter((alias) => alias != cmdTriggers[0]);
+  };
 
-    Tritium.helpThisPoorMan = (msg, cmd) =>
-        Tritium.getCommand("help").run({ Tritium, message: msg, args: cmd.triggers[0] });
+  /**
+   * Returns a string with the number in its ordinal form
+   */
+  Number.prototype.toOrdinal = function () {
+    return this.toString().slice(-1) === "1"
+      ? `${this}st`
+      : this.toString().slice(-1) === "2"
+      ? `${this}nd`
+      : this.toString().slice(-1) === "3"
+      ? `${this}rd`
+      : `${this}th`;
+  };
 
-    process.on("uncaughtException", (err) => {
-        const cleanErrorMsg = err.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
-        //Tritium.logger
-        console.log("error", `Uncaught Exception: ${cleanErrorMsg}`);
-        process.exit(1);
+  /**
+   * Returns a random value from the given array
+   */
+  Array.prototype.getRandom = function () {
+    return this[Math.floor(Math.random() * (this.length - 0 + 1)) + 0];
+  };
+
+  /**
+   * Scrambles and returns the given array
+   */
+  Array.prototype.toScrambled = function () {
+    return this.sort(() => Math.random() - 0.5);
+  };
+
+  Tritium.helpThisPoorMan = (msg, cmd) =>
+    Tritium.getCommand("help").run({
+      Tritium,
+      message: msg,
+      args: Tritium.getCommand(cmd).triggers[0],
     });
 
-    process.on("unhandledRejection", (err) => {
-        //Tritium.logger
-        console.log("error", `Unhandled rejection: ${err}`);
-    });
+  process.on("uncaughtException", (error) => {
+    const cleanErrorMsg = error.stack.replace(new RegExp(`${__dirname}/`, "g"), "./");
+    //Tritium.logger
+    console.log("error", `Uncaught Exception: ${cleanErrorMsg}`);
+    process.exit(1);
+  });
+
+  process.on("unhandledRejection", (error) => {
+    //Tritium.logger
+    console.log("error", `Unhandled rejection: ${error}`);
+  });
 };
--- utils/launch_options.js
+++ utils/launch_options.js
@@ -1,22 +1,26 @@
-module.exports = options = (headless, start) => {
-    const options = {
-        sessionId: "tritiumCarteVitale",
-        headless: headless,
-        autoRefresh: true,
-        restartOnCrash: start,
-        cacheEnabled: false,
-        //executablePath: execPath,
-        //useChrome: true,
-        killProcessOnBrowserClose: true,
-        throwErrorOnTosBlock: false,
-        chromiumArgs: [
-            "--no-sandbox",
-            "--disable-setuid-sandbox",
-            "--aggressive-cache-discard",
-            "--disable-cache",
-            "--disable-application-cache",
-            "--disable-offline-load-stale-cache",
-            "--disk-cache-size=0" /*,
+/**
+ * @param {boolean} headless Whether run in headless mode or not
+ * @param {function} callback Callback function if client crashes
+ */
+module.exports = options = (headless, callback) => {
+  const options = {
+    sessionId: "tritiumCarteVitale",
+    headless: headless,
+    autoRefresh: true,
+    restartOnCrash: callback,
+    cacheEnabled: false,
+    //executablePath: execPath,
+    //useChrome: true,
+    killProcessOnBrowserClose: true,
+    throwErrorOnTosBlock: false,
+    chromiumArgs: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--aggressive-cache-discard",
+      "--disable-cache",
+      "--disable-application-cache",
+      "--disable-offline-load-stale-cache",
+      "--disk-cache-size=0" /*,
             '--disable-breakpad',
             '--disable-component-update',
             '--disable-hang-monitor',
@@ -26,8 +30,8 @@ module.exports = options = (headless, start) => {
             '--disable-metrics-reporting',
             '--disable-dev-tools',
             '--ssl-version-min=tl'*/,
-        ],
-    };
+    ],
+  };
 
-    return options;
+  return options;
 };
